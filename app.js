e4512c0993931567f70443468b9c7616ba3a22bb
Sushil Shinde <shinde.sushil@gmail.com>
2020-04-27 15:06:28 +0530

    added new endpoint

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/users');
 
 var v1ApiRouter = require('./routes/v1Api');
+var webhookRouter = require('./routes/webhooks');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/v1/api', v1ApiRouter);
+app.use('/webhooks', webhookRouter);
 //app.use(compression())
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
